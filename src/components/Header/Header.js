7b06f1fe812db89ee9d248c62fe2c2785f21c22c
saba rahman <sabarahman.official@gmail.com>
2022-06-03 13:11:44 +0600

    updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
 
 const Header = () => {
     return (
         <div className='header'>
             <h1>Confused Buyer</h1>
-            <h2>Choose 4 Clothes</h2>
+            <h2>Choose Any 4 Clothes</h2>
         </div>
     );
 };
